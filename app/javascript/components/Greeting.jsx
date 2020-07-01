ff40287599e2a4827bcb8c931d1db5749468da64
Alvaro Padilla <varoboss.ap@gmail.com>
2020-07-01 17:57:01 -0500

    Add simple reactjs components for testing

---
+++ app/javascript/components/Greeting.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+export default function Greeting(props) {
+  const [name, setName] = useState('Mary');
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+
+  return (
+    <section>
+      <
+    </section>
+  )
+}
