7dfb159ebe9babe48d02ea9b6d27ea9f03ece05b
Alvaro Padilla <varoboss.ap@gmail.com>
2020-07-06 16:56:45 -0500

    Add user signup functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = 'Todo app with rails and react'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
+
+  def context_phrase(context = '')
+    base_context = 'You are not at signup page'
+    context.empty? ? base_context : context
+  end
 end
